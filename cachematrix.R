1bdcb13a8b414aac055fdca23fb1da2f7d46a082
Anuraggupta81 <Anurag.gupta81@gmail.com>
2016-10-22 15:53:29 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 ## As part of the first function - makeCacheMatrix, i am trying to create matrix
 ## and store it. As input a square matrix is passed, or it can be inserted via a new function
 ## set_matrix. get_matrix will print the existing matrix. SetInverse will set the inverse of a matrix
@@ -38,44 +38,3 @@ cacheSolve <- function(x, ...) {
   x$setInverse(inv)
   inv
 }
-=======
-## As part of the first function - makeCacheMatrix, i am trying to create matrix
-## and store it. As input a square matrix is passed, or it can be inserted via a new function
-## set_matrix. get_matrix will print the existing matrix. SetInverse will set the inverse of a matrix
-## and getInverse will print the inverse of the matrix.
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set_matrix <- function(y){
-    x <<- y
-    inv <<- NULL
-  }
-  get_matrix <- function()x
-  setInverse <- function(inverse) inv <<- inverse
-  getInverse <- function()inv
-  list(set_matrix = set_matrix, get_matrix = get_matrix,
-       setInverse = setInverse,
-       getInverse = getInverse)
-  
-}
-
-
-##' cacheSolve function takes in the argument as a square matrix which is being created
-##' as part of previous function.
-##' Need to make use of the function solve for this purpiose which will do the inverse.
-
-cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)) {
-    message("getting cached data")
-    return(inv)
-  }
-  data <- x$get_matrix()
-  inv <- solve(data,...)
-  x$setInverse(inv)
-  inv
-}
->>>>>>> origin/master
